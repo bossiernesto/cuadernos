8f6d11889aa1093ea10e502a19a8db7b3f47746f
Ernesto Bossi <bossi.ernestog@gmail.com>
2021-05-10 14:56:04 -0300

    missed url to homepage

--- src/components/Cuaderno/Header.vue
+++ src/components/Cuaderno/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="sidebar-header-component"> 
     <div class="logo flex flex-row py-8 items-center">
-      <a v-bind:href="this.home_url" class="logo block py-5 flex flex-col">
-      <img alt="Vue logo" src="@/assets/images/cuaderno.png" class='w-12'/>
-      </a>
+      <router-link :to="this.home_url" class="logo block py-5 flex flex-col">
+        <img alt="Vue logo" src="@/assets/images/cuaderno.png" class='w-12'/>
+      </router-link>
       <h1 class='text-3xl ml-3 text-gray-700'>{{this.title}}</h1>
     </div>
   </div>
